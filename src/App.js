d6d0087e8c842b9b5dd968537b12ac9280d10ee7
Syed Abil Arqam <abil@Abils-MacBook.local>
2024-03-22 15:18:08 +0000

    Implement get customers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/flims")
+      .then((response) => response.json())
+      .then((data) => setCustomers(data))
+      .catch((error) => console.error("Error fetching customers:", error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from Sakila Client!</h1>
+      <h3>Customer List</h3>
+      <ul>
+        {customers.map((customer) => (
+          <li key={customer.customer_id}>{customer.first_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
